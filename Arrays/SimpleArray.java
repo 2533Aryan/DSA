a47b41697a02af57f3b2c83951b4e7123db57428
2533Aryan <2533aryan@gmail.com>
2024-07-31 08:02:14 -0400

    init simpleArray

---
+++ Arrays/SimpleArray.java
@@ -0,0 +1,40 @@
+public class SimpleArray<T> implements SimpleArrayInterface<T> {
+
+    @Override
+    public void add(T element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public T get(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public void set(int index, T element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'set'");
+    }
+
+    @Override
+    public void remove(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+    
+
+}
--- Arrays/SimpleArrayInterface.java
+++ Arrays/SimpleArrayInterface.java
@@ -1,5 +1,9 @@
-package Arrays;
-
-public class SimpleArrayInterface {
-    
+// SimpleArrayInterface.java
+public interface SimpleArrayInterface<T> {
+    void add(T element);        // Add an element to the array
+    T get(int index);          // Get an element at a specific index
+    void set(int index, T element);  // Set an element at a specific index
+    void remove(int index);    // Remove an element at a specific index
+    int size();                // Get the number of elements in the array
+    boolean isEmpty();         // Check if the array is empty
 }
