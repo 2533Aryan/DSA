42537a6f0b74fa04635f4f9ec1a1c5901b5cd169
2533Aryan <2533aryan@gmail.com>
2024-08-03 08:35:43 -0400

    insert and get

--- Arrays/DynamicArray/DynamicArray.java
+++ Arrays/DynamicArray/DynamicArray.java
@@ -6,30 +6,53 @@ public class DynamicArray<T> implements ArrayInterface<T> {
     private T[] array;
     private static final int CAPACITY = 10;
 
+
     @SuppressWarnings("unchecked")
     DynamicArray(){
         array = (T[]) new Object[CAPACITY];
         size = 0;
     }
-    
+
+
     @Override
     public void add(T element) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        if(size == array.length){
+            resize();
+        }
+
+        array[size++] = element; 
     }
+
 
     @Override
     public void insert(int index, T element) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+        if (index < 0 || index > size){
+            throw new IndexOutOfBoundsException("Out of Bound");
+        }
+
+        if (index > array.length){
+            resize();
+        }
+
+        for(int i = size; i <= index; i--){
+            array[i+1] = array[i];
+        }
+
+        array[index] = element;
+        size++;
     }
+
 
     @Override
     public T get(int index) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'get'");
+        if (index < 0 || index >= size){
+            throw new IndexOutOfBoundsException("Out of Bound");
+        }
+
+        return array[index];
     }
 
+    
     @Override
     public void set(int index, T element) {
         // TODO Auto-generated method stub
